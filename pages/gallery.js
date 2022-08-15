1c200ce6027c1a7b3e65c029152252cb7dc96e15
fodela <particleme30@gmail.com>
2022-08-15 04:30:42 +0000

    feat: gallery page

---
+++ pages/gallery.js
@@ -0,0 +1,13 @@
+const gallery = () => {
+	return (
+		<div className="grid grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4">
+			<img src="gallery/gallery-1.jpg" alt="gallery-img" />
+			<img src="gallery/gallery-1.jpg" alt="gallery-img" />
+			<img src="gallery/gallery-1.jpg" alt="gallery-img" />
+			<img src="gallery/gallery-1.jpg" alt="gallery-img" />
+			<img src="gallery/gallery-1.jpg" alt="gallery-img" />
+			<img src="gallery/gallery-1.jpg" alt="gallery-img" />
+		</div>
+	);
+};
+export default gallery;
